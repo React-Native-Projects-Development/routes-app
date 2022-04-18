c93fd103f61c44d6aa9ea27fb8881aa3227642b8
Brayan Garcia <brayan@dreamcode.io>
2022-04-18 12:43:42 -0600

    conditionally display routes

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {MapScreen} from '../screens/MapScreen';
 import {PermissionsScreen} from '../screens/PermissionsScreen';
+import {PermissionsContext} from '../context/PermissionsContext';
+import {LoadingScreen} from '../screens/LoadingScreen';
 
 const Stack = createStackNavigator();
 
 export const Navigator = () => {
+  const {permissions} = useContext(PermissionsContext);
+
+  if (permissions.locationStatus === 'unavailable') {
+    return <LoadingScreen />;
+  }
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -16,8 +24,11 @@ export const Navigator = () => {
           backgroundColor: 'white',
         },
       }}>
-      <Stack.Screen name="Settings" component={PermissionsScreen} />
-      <Stack.Screen name="MapScreen" component={MapScreen} />
+      {permissions.locationStatus === 'granted' ? (
+        <Stack.Screen name="MapScreen" component={MapScreen} />
+      ) : (
+        <Stack.Screen name="Settings" component={PermissionsScreen} />
+      )}
     </Stack.Navigator>
   );
 };
